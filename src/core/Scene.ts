43dae6b5fea9669d48b453c207240f07a5702b90
RealAbdurRehman <claudethebitplayer@gmail.com>
2026-08-27 17:04:52 +0500

    Update lighting

--- src/core/Scene.ts
+++ src/core/Scene.ts
@@ -9,13 +9,16 @@ export default class Scene {
     this.init();
   }
   private init(): void {
-    this.instance.background = new THREE.Color(0x000000);
+    this.instance.background = new THREE.Color(0x1a1c23);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+    const hemiLight = new THREE.HemisphereLight(0x87ceeb, 0x444444, 0.6);
+    this.instance.add(hemiLight);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
     this.instance.add(ambientLight);
 
-    const keyLight = new THREE.DirectionalLight(0xffffff, 1.3);
-    keyLight.position.set(4, 6, 5);
+    const keyLight = new THREE.DirectionalLight(0xffffff, 1.5);
+    keyLight.position.set(4, 8, 5);
     this.instance.add(keyLight);
 
     enableLightShadow({ light: keyLight });
